7e6bafd4a8c26a1eae32e361247e0c04bb8ac525
Kunal Pawar <drsubhashpawar550@gmail.com>
2025-05-15 11:31:23 +0530

    Add files via upload

---
+++ to find cube of a number.js
@@ -0,0 +1,8 @@
+// Kunal Pawar
+console.log('Hello I am kunal.');
+console.log('Today I wil tell you cube of choosen number.');
+const choosenValue = prompt('Choose a number between 1 to 100: ');
+const cubeValue = (choosenValue * choosenValue * choosenValue) ;
+console.log(
+  `The cube of a number is ${cubeValue}`
+);
